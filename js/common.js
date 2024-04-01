d7fb01fdc6ed78ce8ef1db57eeb2ffd68443f3f6
simaryadav <63393050+simaryadav@users.noreply.github.com>
2024-04-01 10:51:10 -0400

    Update common.js

--- js/common.js
+++ js/common.js
@@ -173,15 +173,6 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 
-
-  // =====================
-  // Load More Posts
-  // =====================
-  var load_posts_button = document.querySelector('.load-more-posts');
-
-  load_posts_button && load_posts_button.addEventListener("click", function (e) { e.preventDefault(); var o = document.querySelector(".pagination"), e = pagination_next_url.split("/page")[0] + "/page/" + pagination_next_page_number + "/"; fetch(e).then(function (e) { if (e.ok) return e.text() }).then(function (e) { var n = document.createElement("div"); n.innerHTML = e; for (var t = document.querySelector(".grid"), a = n.querySelectorAll(".article__grid"), i = 0; i < a.length; i++)t.appendChild(a.item(i)); new LazyLoad({ elements_selector: ".lazy" }); pagination_next_page_number++, pagination_next_page_number > pagination_available_pages_number && (o.style.display = "none") }) });
-
-
   /* =================================
   // Accordion
   ================================= */
